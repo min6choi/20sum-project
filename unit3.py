1cabe8d46ed583ddab706ca4e66910abdb95f452
Minsik Choi <57276820+min6choi@users.noreply.github.com>
2020-07-15 15:08:08 +0900

    Delete unit3.py

--- unit3.py
+++
@@ -1,8 +0,0 @@
-Python 3.8.4 (v3.8.4:dfa645a65e, Jul 13 2020, 10:45:06) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> print('Hello, world!')
-Hello, world!
->>> print('Hello, world!')
-Hello, world!
->>> 
